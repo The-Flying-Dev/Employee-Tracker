0fa56e93f128fc6673b8a68ece316306250ac75d
The-Flying-Dev <leandranm@outlook.com>
2022-09-22 18:36:47 -0700

    Add highlight for active link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 module ApplicationHelper
   include Pagy::Frontend
+
+  #highlight active link
+  def active_link_to(name, path)
+    content_tag(:li, class: "#{'active fw-bold' if current_page?(path)} nav-item") do
+      link_to name, path, class: "nav-link"    
+    end 
+  end 
+ 
 end
